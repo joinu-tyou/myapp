33b83c1953f65db16bfd35d6afa28997f188a5df
joinus.tyou <joinus.tyou@gmail.com>
2023-11-11 17:14:05 +0900

    テーブル作成：rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_11_024030) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,21 @@ ActiveRecord::Schema[7.1].define(version: 0) do
     t.interval "len"
 
     t.unique_constraint ["date_prod"], name: "production"
+  end
+
+  create_table "samples", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "provider"
+    t.string "ui"
+    t.string "name"
+    t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
